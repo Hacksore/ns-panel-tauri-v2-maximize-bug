8f87a50fe392f37e1301ed6f842c93d9b669a162
Victor Aremu <me@victorare.mu>
2024-09-13 13:40:04 +0100

    fix: the issue that causes app crashing (#1)

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 import "./App.css";
 
 function App() {
-
   return (
     <div data-tauri-drag-region className="container">
-      <h1 data-tauri-drag-region style={{ display: "flex", alignItems: "center", justifyContent: "center", height: 80, width: "100%", backgroundColor: "black" }}>Double Click Me!</h1>
-
+      <h1
+        data-tauri-drag-region
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          height: 80,
+          width: "100%",
+          backgroundColor: "black",
+        }}
+      >
+        Double Click Me!
+      </h1>
     </div>
   );
 }
