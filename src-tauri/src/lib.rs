c3d3007379aa805daec292d5d457a4bbb7d9025b
Sean Boult <996134+Hacksore@users.noreply.github.com>
2024-09-13 07:53:48 -0500

    Revert "fix: the issue that causes app crashing (#1)"
    This reverts commit 8f87a50fe392f37e1301ed6f842c93d9b669a162.

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,56 +1,49 @@
 #![cfg_attr(
-    all(not(debug_assertions), target_os = "windows"),
-    windows_subsystem = "windows"
+  all(not(debug_assertions), target_os = "windows"),
+  windows_subsystem = "windows"
 )]
 
 use tauri::{AppHandle, Manager, WebviewWindow};
 use tauri_nspanel::{panel_delegate, WebviewWindowExt};
 
 pub fn run() {
-    tauri::Builder::default()
-        .plugin(tauri_nspanel::init())
-        .setup(|app| {
-            init(app.app_handle());
+  tauri::Builder::default()
+    .plugin(tauri_nspanel::init())
+    .setup(|app| {
+      init(app.app_handle());
 
-            Ok(())
-        })
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+      Ok(())
+    })
+    .run(tauri::generate_context!())
+    .expect("error while running tauri application");
 }
 
 fn init(app_handle: &AppHandle) {
-    let window: WebviewWindow = app_handle.get_webview_window("main").unwrap();
+  let window: WebviewWindow = app_handle.get_webview_window("main").unwrap();
 
-    let panel = window.to_panel().unwrap();
+  let panel = window.to_panel().unwrap();
 
-    let delegate = panel_delegate!(MyPanelDelegate {
-        window_did_become_key,
-        window_did_resign_key
-    });
+  let delegate = panel_delegate!(MyPanelDelegate {
+    window_did_become_key,
+    window_did_resign_key
+  });
 
-    let handle = app_handle.to_owned();
+  let handle = app_handle.to_owned();
 
-    delegate.set_listener(Box::new(move |delegate_name: String| {
-        match delegate_name.as_str() {
-            "window_did_become_key" => {
-                let app_name = handle.package_info().name.to_owned();
+  delegate.set_listener(Box::new(move |delegate_name: String| {
+    match delegate_name.as_str() {
+      "window_did_become_key" => {
+        let app_name = handle.package_info().name.to_owned();
 
-                println!("[info]: {:?} panel becomes key window!", app_name);
-            }
-            "window_did_resign_key" => {
-                println!("[info]: panel resigned from key window!");
-            }
-            _ => (),
-        }
-    }));
+        println!("[info]: {:?} panel becomes key window!", app_name);
+      }
+      "window_did_resign_key" => {
+        println!("[info]: panel resigned from key window!");
+      }
+      _ => (),
+    }
+  }));
 
-    // 1. Define a non activating window style mask
-
-    #[allow(non_upper_case_globals)]
-    pub const NSWindowStyleMaskNonActivatingPanel: i32 = 1 << 7;
-
-    // 2. Prevents the panel from activating the app
-    panel.set_style_mask(NSWindowStyleMaskNonActivatingPanel);
-
-    panel.set_delegate(delegate);
+  panel.set_delegate(delegate);
 }
+
